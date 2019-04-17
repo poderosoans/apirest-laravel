5d0bf60d2149a2bd0141ac844483547c2e2461d4
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-16 20:14:42 -0500

    [INFO] - Retornando el error de modelo no encontrado como JSON

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use App\Traits\ApiResponser;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -51,6 +52,11 @@ class Handler extends ExceptionHandler
     {
         if($exception instanceof ValidationException) {
             return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+
+        if($exception instanceof ModelNotFoundException) {
+            $model = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("No existe ninguna instancia de {$model} con el id especificado", 404);
         }
 
         return parent::render($request, $exception);
