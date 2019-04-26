965f7635993a5075e422c7bcf0bce4ff69b416d8
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-26 11:45:19 -0500

    [INFO] - Ordenado y Filtrado: Filtrando respuestas según múltiples atributos.

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -21,6 +21,7 @@ trait ApiResponser {
 		}
 		$transformer = $collection->first()->transformer;
 
+		$collection = $this->filterData($collection,$transformer);
 		$collection = $this->sortData($collection,$transformer);
 
 		$collection = $this->transformData($collection, $transformer);
@@ -51,6 +52,18 @@ trait ApiResponser {
 
 			$collection = $collection->sortBy->{$attribute};
 		}
+		return $collection;
+	}
+
+	protected function filterData(Collection $collection, $transformer) {
+		foreach (request()->query() as $query => $value) {
+			$attribute = $transformer::originalAttribute($query);
+			
+			if(isset($attribute, $value)) {
+				$collection = $collection->where($attribute, $value);
+			}
+		}
+
 		return $collection;
 	}
 
