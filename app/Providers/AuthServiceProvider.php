4edc7aa38229dd26c2e2da43d5ecbd6af287b1f2
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-30 16:05:43 -0500

    [INFO] - Seguridad con Passport (OAuth2): Implementando el grant type implícito.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,5 +30,6 @@ class AuthServiceProvider extends ServiceProvider
         Passport::routes(null, ['prefix' => 'api/oauth']);
         Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+        Passport::enableImplicitGrant();
     }
 }
