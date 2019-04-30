c837c1213cab4a38c60f883c4918bd4b4e16b35b
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-30 17:36:01 -0500

    [INFO] - Seguridad con Passport (OAuth2-Scopes): Restringiendo acciones en los controlladores para que usen los scopes creados.

--- app/Http/Controllers/Product/ProductBuyerTransactionController.php
+++ app/Http/Controllers/Product/ProductBuyerTransactionController.php
@@ -16,6 +16,7 @@ class ProductBuyerTransactionController extends ApiController
     public function __construct() {
         parent::__construct();
         $this->middleware('transform.input:' . TransactionTransformer::class)->only(['store']);
+        $this->middleware('scope:puchase-product')->only('store');
     }
 
     /**
--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Storage;
 use App\Transformers\ProductTransformer;
+use Illuminate\Auth\AuthenticationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
@@ -17,6 +18,7 @@ class SellerProductController extends ApiController
     public function __construct() {
         parent::__construct();
         $this->middleware('transform.input:' . ProductTransformer::class)->only(['store', 'update']);
+        $this->middleware('scope:manage-products')->except('index');
     }
 
     /**
@@ -26,8 +28,13 @@ class SellerProductController extends ApiController
      */
     public function index(Seller $seller)
     {
-        $products = $seller->products;
-        return $this->showAll($products);
+        if(request()->user()->tokenCan('read-general') || request()->user()->tokenCan('manage-products')) {
+            $products = $seller->products;
+
+            return $this->showAll($products);
+
+        }
+        throw new AuthenticationException;
     }
 
 
