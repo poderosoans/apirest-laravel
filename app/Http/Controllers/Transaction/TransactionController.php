762d3532768f4cd0384dd4aeadc67469df267691
Roger Pomacarhua <poderosoans@hotmail.com>
2019-05-01 12:25:35 -0500

    [INFO] - Seguridad con Polices y Gates: Implementando restricciones sobre Transactions y Product.

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -12,6 +12,7 @@ class TransactionController extends ApiController
     public function __construct() {
         parent::__construct();
         $this->middleware('scope:read-general')->only('show');
+        $this->middleware('can:view,transaction')->only('show');
     }
     /**
      * Display a listing of the resource.
